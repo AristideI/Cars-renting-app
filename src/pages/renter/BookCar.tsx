0ebe9a357c140f4bcd8fdce9ee949b183edf1934
AristideI <a.isingizwe@alustudent.com>
2023-10-25 08:05:45 +0200

    updates

--- src/pages/renter/BookCar.tsx
+++ src/pages/renter/BookCar.tsx
@@ -1,3 +1,40 @@
+import { useCarTypes, useCars, useUser, useUsers } from '../../hooks'
+import { UserDto } from '../../util/api'
+
+interface Axios {
+  loading: boolean
+  error: unknown
+  data: UserDto[]
+}
 export default function BookCar() {
-  return <h1>Book car</h1>
+  const [fetchAllCars] = useCars()
+  const [fetchCarTypes] = useCarTypes()
+  const [fetchUsers] = useUsers()
+  const [fetchUser] = useUser(2)
+  console.log(fetchUser)
+  if (
+    fetchAllCars.loading === true ||
+    fetchCarTypes.loading === true ||
+    fetchUsers.loading === true
+  ) {
+    return (
+      <section className="grid h-[90vh] place-content-center overflow-hidden font-lora text-3xl text-white">
+        LOADING ...
+      </section>
+    )
+  }
+
+  const allCars = fetchAllCars.data
+  const carTypes = fetchCarTypes.data
+  const users = fetchUsers.data
+  // console.log(allCars, carTypes, users)
+  return (
+    <article className="text-lightGray">
+      <section className="mt-8 flex items-center justify-between">
+        <img src="/Back.png" alt="Back icon" />
+        <p className="font-lora text-2xl">All Cars</p>
+        <div className="h-1 w-2"></div>
+      </section>
+    </article>
+  )
 }
